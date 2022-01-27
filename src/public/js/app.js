5d02903e92f482d515a1fd45786a04639f19ec7e
gameisboring <daum128@naver.com>
2022-01-27 17:02:33 +0900

    3

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,27 +1,72 @@
 const socket = io()
+
 const byeBtn = document.querySelector('#byeBtn')
 const welcomeBtn = document.querySelector('#welcomeBtn')
-const form = welcome.querySelector('form')
-const room = document.querySelector('#room')
-function handleRoomSubmit(event) {
-  event.preventDefault()
-  const input = form.querySelector('input')
+const sendBtn = document.querySelector('#sendBtn')
 
-  socket.emit('enter_room', input.value, (msg) => {
-    alert(`${msg}`)
-  })
+const welcome = document.querySelector('#welcome')
+const room = document.querySelector('#room')
+
+const welcomeForm = welcome.querySelector('form')
+const roomForm = room.querySelector('form')
+
+room.hidden = true
+let roomName, message
+
+function handleEnterRoomSubmit(event) {
+  event.preventDefault()
+  const input = welcomeForm.querySelector('input')
+  roomName = input.value
   input.value = ''
+
+  socket.emit('enter_room', input.value, showRoom)
 }
 
 function handleOutRoomSubmit(event) {
   event.preventDefault()
-  const input = form.querySelector('input')
 
-  socket.emit('out_room', input.value, (msg) => {
-    alert(`${msg}`)
-  })
-  input.value = ''
+  socket.emit('out_room', roomName, hideRoom)
+  roomName = ''
 }
 
-welcomeBtn.addEventListener('click', handleRoomSubmit)
+function showRoom() {
+  welcome.hidden = true
+  room.hidden = false
+  const h3 = room.querySelector('h3')
+  h3.innerText = roomName
+}
+
+function handleSendSubmit(event) {
+  event.preventDefault()
+
+  const input = roomForm.querySelector('input[type=text]')
+  message = input.value
+  socket.emit('send_message', message)
+  console.log(message)
+}
+
+function hideRoom() {
+  welcome.hidden = false
+  room.hidden = true
+  const h3 = room.querySelector('h3')
+  h3.innerText = ''
+}
+
+welcomeBtn.addEventListener('click', handleEnterRoomSubmit)
 byeBtn.addEventListener('click', handleOutRoomSubmit)
+roomForm.addEventListener('submit', handleSendSubmit)
+
+function addMessage(message) {
+  const ul = room.querySelector('ul')
+  const li = document.createElement('li')
+  li.innerText = message
+  ul.append(li)
+}
+/* socket.on("이벤트 이름", "콜백") */
+socket.on('welcome', () => {
+  addMessage('Someone Joined')
+})
+
+socket.on('bye', () => {
+  addMessage('Someone Lefted')
+})
